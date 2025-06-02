dc24bd29d4e22afafa6e923ce99da8769e0ea179
brogers5 <6869577+brogers5@users.noreply.github.com>
2025-06-02 18:31:28 -0400

    Add missing update code

--- update.ps1
+++ update.ps1
@@ -1,7 +1,38 @@
 Import-Module AU
 
+function Get-InstallScript($FilePath) {
+    if (!(Get-Command 'innounp.exe' -ErrorAction SilentlyContinue)) {
+        Write-Information 'innounp is not available on PATH, installing...'
+        choco install innounp -y
+    }
+
+    $installScriptFileName = 'install_script.iss'
+    innounp -x $FilePath $installScriptFileName -y
+}
+
+function Set-DocumentVersion($RelativeFilePath) {
+    $fileContents = Get-Content -Path $RelativeFilePath -Encoding UTF8
+    $fileContents = $fileContents -replace '/tree/v.*\/', "/tree/v$($Latest.Version)/"
+
+    $encoding = New-Object System.Text.UTF8Encoding($false)
+    $output = $fileContents | Out-String
+    $absoluteFilePath = (Get-Item -Path $RelativeFilePath).FullName
+    [System.IO.File]::WriteAllText($absoluteFilePath, $output, $encoding)
+}
+
 function global:au_BeforeUpdate ($Package) {
-    Set-DescriptionFromReadme -Package $Package -ReadmePath 'DESCRIPTION.md'
+    $tempFilePath = New-TemporaryFile
+    Invoke-WebRequest -Uri $Latest.Url32 -OutFile $tempFilePath
+
+    $Latest.Checksum32 = (Get-FileHash -Path $tempFilePath -Algorithm SHA256).Hash.ToLower()
+    Get-InstallScript -FilePath $tempFilePath
+
+    Remove-Item $tempFilePath -Force
+
+    $descriptionRelativePath = '.\DESCRIPTION.md'
+    Set-DocumentVersion -RelativeFilePath $descriptionRelativePath
+
+    Set-DescriptionFromReadme -Package $Package -ReadmePath $descriptionRelativePath
 }
 
 function global:au_SearchReplace {
