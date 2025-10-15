b86b74bce18bc04921843bcf1f7c5c8377ac93a5
brogers5 <6869577+brogers5@users.noreply.github.com>
2025-10-14 20:59:37 -0400

    Create package fix version to update checksum

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'EXE'
   softwareName   = 'CPUID HWMonitor *'
   url            = 'https://download.cpuid.com/hwmonitor/hwmonitor_1.60.exe'
-  checksum       = 'efb5e43fcdee8756228952ea2b088bc982be88c2b597b035ece973c9e4188c33'
+  checksum       = '1cab5767446ad33c3a23811e359f02739ce6d4d65ca600a90db32a1c0c9088a4'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).Install.log`""
   validExitCodes = @(0)
