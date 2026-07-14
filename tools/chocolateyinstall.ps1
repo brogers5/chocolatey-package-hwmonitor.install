573a6be2a88bd31d6d6a24d97fe349c3914727ed
brogers5 <6869577+brogers5@users.noreply.github.com>
2026-07-14 17:56:44 -0400

    Update to 1.65.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   softwareName   = 'CPUID HWMonitor *'
-  url            = 'https://download.cpuid.com/hwmonitor/hwmonitor_1.64.exe'
-  checksum       = 'e64d0345905d5034441c9c50c447ab831bad35062b811ccb0c66c68fa86b36f7'
+  url            = 'https://download.cpuid.com/hwmonitor/hwmonitor_1.65.exe'
+  checksum       = '14f46de20cbd1d166608f74e5774aeb2c52c59f2dade31c23c28a67cda5b32c9'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).Install.log`""
   validExitCodes = @(0)
